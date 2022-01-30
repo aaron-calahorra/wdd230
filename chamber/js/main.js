de5308201df57725c032477c8e213e48969b557c
aaron-calahorra <cal21028@byui.edu>
2022-01-29 23:36:19 -0700

    Update

---
+++ chamber/js/main.js
@@ -0,0 +1,29 @@
+const datefieldUK = document.querySelector("#date");
+
+const now = new Date();
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+datefieldUK.innerHTML = `${fulldateUK}`;
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+/*** Programming Notes **************************************
+  Arrow Functions - es6 syntactically compact alternative to a regular function expression
+  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+  or https://www.w3schools.com/js/js_arrow_function.asp
+
+  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+  */
+
+  document.querySelector("#copyright-year").innerText = new Date().getFullYear();
+
+document.querySelector("#last-modified-date").innerText = new Date(document.lastModified);
