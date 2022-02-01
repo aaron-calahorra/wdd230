ac3f1a5f42f2489dbde5d4ff99764fde337a4a2d
aaron-calahorra <cal21028@byui.edu>
2022-02-01 10:56:39 -0700

    Lesson 5 Update

---
+++ lesson5/js/main.js
@@ -0,0 +1,26 @@
+const input = document.querySelector("input");
+
+const button = document.querySelector("button");
+
+const list = document.querySelector("ul");
+
+button.onclick = function() {
+    let myChapter = input.value;
+    input.value = "";
+
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listBtn = document.createElement("button");
+
+    listItem.appendChild(listText);
+    listText.textContent = myChapter;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = "❌";
+    list.appendChild(listItem);
+
+    listBtn.onclick = function(e) {
+        list.removeChild(listItem);
+    }
+
+    input.focus();
+}
