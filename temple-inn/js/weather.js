135521edc39f89e47d8f1594bcda378d9359d5e1
aaron-calahorra <cal21028@byui.edu>
2022-04-05 21:11:04 -0600

    Update

--- temple-inn/js/weather.js
+++ temple-inn/js/weather.js
@@ -18,11 +18,19 @@ fetch(apiURL)
     
 });
 
-const foreURL = "https://api.openweathermap.org/data/2.5/forecast?id=4015076&appid=d1b2b0b463523d6cf47a9c4e7eb0de0e&units=metric&cnt=3"
+const foreURL = "https://api.openweathermap.org/data/2.5/forecast?id=4015076&appid=d1b2b0b463523d6cf47a9c4e7eb0de0e&units=metric&cnt=24"
 
 fetch(foreURL)
   .then((response2) => response2.json())
   .then((jsObject2) => {
     console.log(jsObject2);
-    
+    document.querySelector("#day1").textContent = new Date(jsObject2.list[6].dt_txt).toLocaleDateString("en-US", { month: "short", day: "numeric" });
+    document.querySelector("#day2").textContent = new Date(jsObject2.list[14].dt_txt).toLocaleDateString("en-US", { month: "short", day: "numeric" });
+    document.querySelector("#day3").textContent = new Date(jsObject2.list[22].dt_txt).toLocaleDateString("en-US", { month: "short", day: "numeric" });
+    document.querySelector("#day1-temp").textContent = Math.round(jsObject2.list[6].main.temp) + "\xB0C";
+    document.querySelector("#day2-temp").textContent = Math.round(jsObject2.list[14].main.temp) + "\xB0C";
+    document.querySelector("#day3-temp").textContent = Math.round(jsObject2.list[22].main.temp) + "\xB0C";
+    document.querySelector("#day1-desc").textContent = jsObject2.list[6].weather[0].main;
+    document.querySelector("#day2-desc").textContent = jsObject2.list[14].weather[0].main;
+    document.querySelector("#day3-desc").textContent = jsObject2.list[22].weather[0].main;
 });
